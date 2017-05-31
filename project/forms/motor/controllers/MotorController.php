a24decfc0042a333599d7f08acf03fe9883d3135
Samuel Dervis <samueldervis@gmail.com>
2017-05-31 18:53:11 +0300

    Updates for controller

--- project/forms/motor/controllers/MotorController.php
+++ project/forms/motor/controllers/MotorController.php
@@ -49,31 +49,45 @@ class MotorController extends Controller
      */
     private $_quotes;
 
-    public function setEntities()
+    /**
+     * @var ProductsController
+     */
+    private $_products;
+
+    /**
+     * Setup entities. Via Elements call
+     */
+    private function setEntities()
     {
         $this->_customer = Elements::call('Customers/CustomersController');
         $this->_entity = Elements::call('Entities/EntitiesController');
         $this->_quotes = Elements::call('Quotes/QuotesController');
+        $this->_products = Elements::call('Products/ProductsController');
     }
 
     /**
+     * Default entry point for external form. Switch between display forms depending on the step. [1 to 4]
      * @param string $step
      */
     public function index($step = "1")
     {
+        //SNGUMO - Removed redirection because for new customer the user_id isnt set
+
 //        if ($step != "1") {
 //            if (!Session::has('user_id')) {
 //                Redirect::to('/motor/step/1');
 //                exit;
 //            }
 //        }
+
         $this->setEntities();
         $this->loadData($step);
         $this->view->wizard($this->data);
     }
 
     /**
-     * @param int $step
+     * Save incoming forms posted from the front end
+     * @param $step
      */
     public function save($step)
     {
@@ -99,11 +113,13 @@ class MotorController extends Controller
     }
 
     /**
-     * Load data for the view
+     * Load View Data
+     * Set up some data needed in the View and panels
      * @param $step
      */
     private function loadData($step)
     {
+        $this->setEntities();
         $this->data = new \stdClass();
         $this->data->step = $step;
         $this->data->titles = ['Mr' => 'Mr', 'Mrs' => 'Mrs', 'Ms' => 'Ms', 'Dr' => 'Dr', 'Prof' => 'Prof', 'Eng' => 'Eng'];
@@ -130,12 +146,11 @@ class MotorController extends Controller
         }
         $this->data->makes = $make;
         $this->data->cover_type = $this->__select(['Comprehensive', 'Third Party Fire and Theft', 'Third Party Only']);
+
         if ($step == 4) {
-            $the_quote = $this->_quotes->getQuoteById(Session::get('quote_id'));
-//            $jubilee = (new JubileeQuotes($the_quote))->calculate();
-//            $aig = (new AIGQuotes($the_quote))->calculate();
-            $this->data->payments = (new JubileeQuotes($the_quote))->calculate();
+            $this->data->payments = $this->_quotes->getQuotations(Session::get('quote_id'));
         }
+
         $this->data->pick_cert = $this->__select([
             'Nairobi, Head Office, Jubilee Insurance House, Wabera Street',
             'Nairobi, Mombasa Road, Tulip House, Ground Floor',
@@ -150,29 +165,35 @@ class MotorController extends Controller
             'Nyeri, Sohan Plaza, Moi Nyayo Way',
             'Thika, Thika Arcade, Thika Town, 4th Floor',
             'Post to my address in your records (a small extra charge will apply)']);
+
         $this->data->customer = $this->_customer->getCustomerByUserId($this->user()->user_id);
     }
 
+    /**
+     * Save details of other cars
+     */
     private function saveOtherCarDetails()
     {
         $count = Session::get('other_covers');
         $saved = [];
+        $product_id = $this->_products->getProductByAlias('motor_insurance')->id;
         for ($i = 1; $i <= $count; $i++) {
             $got = $this->__buildStack($i);
             $entity_id = $this->_entity->getEntityIdByAlias('vehicle')->id;
             $saved[] = $this->_entity->saveEntityDataRemotely(Session::get('customer_id'), $entity_id,
-                json_encode($got), null, 1);
+                json_encode($got), $product_id);
 
         }
         Session::set('other_id', serialize($saved));
         Redirect::to('/motor/step/3');
     }
 
+    /**
+     * Save personal details from step 1. Relies on POST
+     */
     private function savePersonalDetails()
     {
-        $saved = $this->_customer->saveCustomer('motor', $this->getInputForStep(1), false);
-
-        if ($saved) {
+        if ($this->_customer->saveCustomer('motor', $this->getInputForStep(1), false)) {
             Session::set('type', 'motor');
 
             $notification = 'Saved Please proceed to step two';
@@ -186,6 +207,11 @@ class MotorController extends Controller
         }
     }
 
+    /**
+     * Attempt to get an array of each cars for extra covers
+     * @param $index
+     * @return array
+     */
     private function __buildStack($index)
     {
         $build = [];
@@ -197,11 +223,16 @@ class MotorController extends Controller
         return $build;
     }
 
+
+    /**
+     * Save car details
+     */
     private function saveCarDetails()
     {
         $entity_id = $this->_entity->getEntityIdByAlias('vehicle')->id;
         $car_details = json_encode($this->getInputForStep(2));
-        $saved = $this->_entity->saveEntityDataRemotely(Session::get('customer_id'), $entity_id, $car_details, null, 1);
+        $product_id = $this->_products->getProductByAlias('motor_insurance')->id;
+        $saved = $this->_entity->saveEntityDataRemotely(Session::get('customer_id'), $entity_id, $car_details, $product_id);
         Session::set('main_id', $saved);
         Session::set('other_id', serialize([]));
         if (!empty(Input::post('othercovers'))) {
@@ -213,6 +244,10 @@ class MotorController extends Controller
 
     }
 
+
+    /**
+     * Save cover details
+     */
     private function saveCoverDetails()
     {
         $ids['customer_id'] = Session::get('customer_id');
@@ -226,6 +261,11 @@ class MotorController extends Controller
     }
 
 
+    /**
+     * Transform an array to its key:value representation
+     * @param $data
+     * @return array
+     */
     private function __select($data)
     {
         $_data = [];
@@ -235,6 +275,11 @@ class MotorController extends Controller
         return $_data;
     }
 
+    /**
+     * Generate some years for the specified span
+     * @param int $span
+     * @return array
+     */
     private function __years($span = 100)
     {
         $year = date('Y');
@@ -246,8 +291,13 @@ class MotorController extends Controller
         return $yearlist;
     }
 
+    /**
+     * Get extra covers schematic
+     * @param int $count
+     */
     public function getExtraCovers($count = 1)
     {
+        $this->setEntities();
         $this->loadData(22);
         echo $this->view->getSchematic($this->data, $count);
     }
